866e0c56eaeab3249e7a14a03f513f5c85b5cdd2
Vishnu Suresh Perumbavoor <63797249+vishnusureshperumbavoor@users.noreply.github.com>
2023-01-21 11:56:53 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@ $name = $_POST["name"];
 $password = $_POST["password"];
 $sql = "insert into phptheory values ('$name','$password');";
 $result = $conn->query($sql);
-if($result==false){
+if(!$result){
     echo "insertion failed";
 }
 else{
